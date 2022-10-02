cec5b8e692fed18d3ad0b32c45f72f130118fcda
Aryen Singhal <42897258+AryenSinghal@users.noreply.github.com>
2022-10-02 22:54:16 +0530

    Reduce time complexity of solution

--- Prelims/Problem5/Solution.py
+++ Prelims/Problem5/Solution.py
@@ -1,24 +1,14 @@
 def max_profit(arr):
-    price=0
-    m=max(arr)
-    s=0
-    j=0
-    for i in range(len(arr)):
-        s=s+arr[i]
-        if arr[i]==m:
-            price=price+((j+1)*m-s)
-            s=0
-            j=0
-            if i!=len(arr)-1:
-                m=max(arr[i+1:])
-        else:
-            j+=1
+    m = arr.pop()
+    maxsum = 0
+    arrsum = 0
+    for p in reversed(arr):
+        m = max(m, p)
+        maxsum += m
+        arrsum += p
+    return maxsum - arrsum
 
-    return price
-            
-
-t=int(input())
-for i in range(t):
-    n=int(input())
-    arr=list(map(int,input().split()))
+for _ in range(int(input())):
+    n = int(input())
+    arr = list(map(int,input().split()))
     print(int(max_profit(arr)))
